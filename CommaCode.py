23af0ad50fe97e8ccd9420eb6fbecedab648de16
Rishi-code757 <rishi.chaturvedi56199@gmail.com>
2025-06-10 09:01:35 +0530

    Create CommaCode.py

---
+++ CommaCode.py
@@ -0,0 +1,14 @@
+print("Rishi Chaturvedi, USN: 1AY24AI089, SEC: O")
+
+def list_to_string(items):
+    if not items:
+        return ''
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f'{items[0]} and {items[1]}'
+    else:
+        return ', '.join(items[:-1]) + ', and ' + items[-1]
+
+spam = ['apples', 'bananas', 'tofu', 'cats']
+print(list_to_string(spam))
