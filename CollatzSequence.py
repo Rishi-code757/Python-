4906bb44664b0699f547b2424fa6b288a62e14a4
Rishi-code757 <rishi.chaturvedi56199@gmail.com>
2025-06-10 08:57:50 +0530

    Create CollatzSequence.py

---
+++ CollatzSequence.py
@@ -0,0 +1,16 @@
+print("Rishi Chaturvedi, USN: 1AY24AI089, SEC: O")
+
+def collatz(number):
+    if number % 2 == 0:
+        result = number // 2
+    else:
+        result = 3 * number + 1
+    print(result)
+    return result
+
+try:
+    user_input = int(input("Enter an integer: "))
+    while user_input != 1:
+        user_input = collatz(user_input)
+except ValueError:
+    print("Please enter a valid integer.")
